158257bb0662f2806bf75e131465a4c6c990904e
Jinbo Ci <32102965+JinboCi@users.noreply.github.com>
2019-06-30 16:36:44 +0800

    debugging arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,31 +1,33 @@
-//本次实验电机驱动采用共阴极接线方式
-//将步进电机的参数写到一个子函数中，并加入限位开关，触动限位开关可实现步进电机停转
+//±¾´ÎÊµÑéµç»úÇý¶¯²ÉÓÃ¹²Òõ¼«½ÓÏß·½Ê½
+//½«²½½øµç»úµÄ²ÎÊýÐ´µ½Ò»¸ö×Óº¯ÊýÖÐ£¬²¢¼ÓÈëÏÞÎ»¿ª¹Ø£¬´¥¶¯ÏÞÎ»¿ª¹Ø¿ÉÊµÏÖ²½½øµç»úÍ£×ª
 
 //20190501 in Zijingang Campus
 
-// 舵机控制
-#include <Servo.h>    // 声明调用Servo.h库
-#include <SoftwareSerial.h>
+// ¶æ»ú¿ØÖÆ
+#include <Servo.h>    // ÉùÃ÷µ÷ÓÃServo.h¿â
+#include "blueTooth.hpp"
 
-
-//define control signal port
+//define control signal port\
 //use PORT_STEPPER_XXXX+1 and PORT_STEPPER_XXXX+2 to get the other two port
-#define PORT_STEPPER_BIG_ARM 8;
-#define PORT_STEPPER_SMALL_ARM 12;
-#define PORT_STEPPER_GIMBAL 16;
-#define PORT_STEPPER_HORIZONTAL 20;
-#define PORT_SERVO 24;
+#define PORT_STEPPER_BIG_ARM 2
+#define PORT_STEPPER_SMALL_ARM 5
+#define PORT_STEPPER_GIMBAL 8
+#define PORT_STEPPER_HORIZONTAL 11
+#define PORT_SERVO 24
 
 //define micro motion switch
-#define SWITCH_STEPPER_BIG_ARM 30;
-#define SWITCH_STEPPER_SMALL_ARM 31;
-#define SWITCH_STEPPER_GIMBAL 32;
-#define SWITCH_STEPPER_HORIZONTAL 33;
-#define SWITCH_SERVO 34;
+#define SWITCH_STEPPER_BIG_ARM 30
+#define SWITCH_STEPPER_SMALL_ARM 31
+#define SWITCH_STEPPER_GIMBAL 32
+#define SWITCH_STEPPER_HORIZONTAL 33
+#define SWITCH_SERVO 34
 
 
-// 创建一个舵机对象
-Servo servo;
+void StepperMotor(int PULPin, boolean DIR, int steps, bool ENA, int velocity=10);
+
+// ´´½¨Ò»¸ö¶æ»ú¶ÔÏó
+Servo myServo;
+
 
 // structure
 struct motor_controler {
@@ -35,156 +37,13 @@ struct motor_controler {
     int target = 0;
   };
   int safe = 0;
-  //int motornum = 5; // the number of motors we want to control at one time we receive data
-  //motor motor_arr[motornum];
   motor stepper_big_arm;
   motor stepper_small_arm;
   motor stepper_gimbal;
   motor stepper_horizontal;
   motor servo;
-} ;
+} MC;
 
-/* function bluetoothsetup 
- *  setup bluetooth
- *  should be called at setup()
- *  
- *  inputs: const int rxpin, const int txpin //Define the pins used for receiving
- *  outputs: bluetooth with type SoftwareSerial
- *  
- *  
- *  
- *  
- */
-SoftwareSerial bluetoothsetup(const int rxpin, const int txpin){
-  
-  //Connect the Bluetooth module
-  SoftwareSerial bluetooth(rxpin, txpin);
-  //Initialize Serial for debugging purposes
-  Serial.begin(9600);
-  Serial.println("Serial ready");
-  //Initialize the bluetooth
-  bluetooth.begin(9600);
-  Serial.println("Bluetooth ready");
-  return bluetooth;
-}
-
-
-
-void setup()
-{
-  // 步进电机
-  // 1是dir 2是pul 3是ena
-  // big_arm
-  //Set the pin to put power out
-  SoftwareSerial bluetooth = bluetoothsetup(0, 1);
-  /* TODO */
-  pinMode (2, OUTPUT);
-  pinMode (3, OUTPUT);
-  pinMode (4, OUTPUT);
-  
-  pinMode (6, OUTPUT);
-  pinMode (7, OUTPUT);
-  pinMode (8, OUTPUT);
-  
-  pinMode (10, OUTPUT);
-  pinMode (11, OUTPUT);
-  pinMode (12, OUTPUT);
-
-  pinMode (14, OUTPUT);
-  pinMode (15, OUTPUT);
-  pinMode (16, OUTPUT);
-
-  // 舵机
-  servo.attach(53);  // 将引脚9上的舵机与声明的舵机对象连接起来
-  
-  struct motor_controler {
-    struct motor{
-      int state = 0;
-      int angle = 0;
-    };
-  int safe = 0;
-  motor stepper_big_arm;
-  motor stepper_small_arm;
-  motor stepper_gimbal;
-  motor stepper_horizontal;
-  motor servo;
-};
-
-
-/*
- * function receiveByte
- * return the data between the StartKey and the EndKey
- * 
- * inputs: SoftwareSerial bluetooth, byte StartKey, byte EndKey
- * outputs byte that we recieved or 0 for failure
- * 
- * 
- */
-
-byte receiveByte (SoftwareSerial bluetooth, byte StartKey, byte EndKey){
-  // if not available, return 0 for failure
-  if (!bluetooth.available()) {
-    return 0;
-  }
-   byte buff = bluetooth.read();
-   // when we cannot get the StartKey, keep recieving
-   while (StartKey != buff){
-     buff = bluetooth.read();
-     delay(1); // I do not know whether this step is necessary
-     bluetooth.write(buff); // send response
-   }
-   byte data = buff;
-   while (EndKey != buff){
-      buff = bluetooth.read();
-      data = buff;
-      delay(1); // I do not know whether this step is necessary
-      bluetooth.write(data); // send response
-   }
-   return data;
-  }
-
-/*
- * function receiveData
- * 
- * inputs: SoftwareSerial bluetooth, byte StartByte, byte InterStart, 
- *         byte InterEnd, byte EndByte, int WaitTime
- * outputs: an int array with array[0] is the total length of the array
- * 
- * attentions:
- * 1) the data would be sent from the computer in the following order:
- * StartByte - Number - InterStart - First Data - InterEnd - 
- * InterStart - Second Data - InterEnd - ... - InterEnd - EndByte
- * 2) the computer would not send the second data, until it recieve the response
- */
-
-
-int * receiveData(SoftwareSerial bluetooth, byte StartByte, byte InterStart, byte InterEnd, byte EndByte){
-  //Check for new data
-  byte buff = receiveByte(bluetooth, StartByte, InterStart);
-  if(buff == 0) {
-    Serial.println("Num Receive Failed");
-    int fai[1];
-    fai[0] = 0;
-    return fai;
-  }
-  int arr[buff+1];
-  for (byte i = 0; i < buff ; i++){
-    buff = receiveByte(bluetooth, InterStart, InterEnd);
-    if(buff == 0) {
-      Serial.println((int)i, "th Receive Failed");
-      arr[0] = 0;
-      break;
-    }
-    if (buff == EndByte) {
-      Serial.println((int)i, "End Unexpectedly at");
-      arr[0] = i+1;
-      break;
-    }
-    arr[i+1] = (int)buff;
-    buff = receiveByte(bluetooth, InterEnd, InterEnd);
-  }
-  return arr;
-}
 
 /**
 * @fun_c check whether each motor is at the initial position
@@ -215,87 +74,55 @@ mc.stepper_small_arm.angle = 0;
 * @params mc: the motor controler
 */
 void check_zero_state (motor_controler& mc) {
-while (!(mc.servo.state&&mc.stepper_big_arm.state&&mc.stepper_gimbal.state&&mc.stepper_horizontal.state&&mc.stepper_small_arm.state)) {
-if (digitalRead(SWITCH_SERVO)==HIGH) {
-mc.servo.angle = 0;
-mc.servo.state = 1;
-StepperMotor(PORT_SERVO, true, 2, true);
-} else {
-StepperMotor(PORT_SERVO, true, 2, false);
-}
-if (digitalRead(SWITCH_STEPPER_BIG_ARM)==HIGH) {
-mc.stepper_big_arm.angle = 0;
-mc.stepper_big_arm.state = 1;
-StepperMotor(PORT_STEPPER_BIG_ARM, true, 2, true);
-} else {
-StepperMotor(PORT_STEPPER_BIG_ARM, true, 2, false);
-}
-if (digitalRead(SWITCH_STEPPER_GIMBAL)==HIGH) {
-mc.stepper_gi
-
-
-
-}
- 
-
-void loop()
-{
-  // 我们假定这个函数可以将蓝牙模块传来的信息弄进我们预先设定的struct里面
-  //BlueToothReceiver();
-  int number_wanted = 4;
-  int * res = receiveData(SoftwareSerial bluetooth, byte StartByte, byte InterStart, byte InterEnd, byte EndByte){
-  if (res[0] != number_wanted)
-  {
-    Serial.println("the num of data recieved does not match with numberwanted");
-    delay(2000);
-    continue;
+  while (!(mc.servo.state&&mc.stepper_big_arm.state&&mc.stepper_gimbal.state&&mc.stepper_horizontal.state&&mc.stepper_small_arm.state)) {
+    if (digitalRead(SWITCH_SERVO)==HIGH) {
+      mc.servo.angle = 0;
+      mc.servo.state = 1;
+      StepperMotor(PORT_SERVO, true, 2, true);
+    } else {
+      StepperMotor(PORT_SERVO, true, 2, false);
+    }
+    if (digitalRead(SWITCH_STEPPER_BIG_ARM)==HIGH) {
+      mc.stepper_big_arm.angle = 0;
+      mc.stepper_big_arm.state = 1;
+      StepperMotor(PORT_STEPPER_BIG_ARM, true, 2, true);
+    } else {
+      StepperMotor(PORT_STEPPER_BIG_ARM, true, 2, false);
+    }
+    if (digitalRead(SWITCH_STEPPER_GIMBAL)==HIGH) {
+      mc.stepper_gimbal.angle = 0;
+      mc.stepper_gimbal.state = 1;
+      StepperMotor(PORT_STEPPER_GIMBAL, true, 2, true);
+    } else {
+      StepperMotor(PORT_STEPPER_GIMBAL, true, 2, false);
+    }
+    
+    if (digitalRead(SWITCH_STEPPER_HORIZONTAL)==HIGH) {
+      mc.stepper_horizontal.angle = 0;
+      mc.stepper_horizontal.state = 1;
+      StepperMotor(PORT_STEPPER_HORIZONTAL, true, 2, true);
+    } else {
+      StepperMotor(PORT_STEPPER_HORIZONTAL, true, 2, false);
+    }
+    
+    if (digitalRead(SWITCH_STEPPER_SMALL_ARM)==HIGH) {
+      mc.stepper_horizontal.angle = 0;
+      mc.stepper_horizontal.state = 1;
+      StepperMotor(PORT_STEPPER_HORIZONTAL, true, 2, true);
+    } else {
+      StepperMotor(PORT_STEPPER_HORIZONTAL, true, 2, false);
+    }
   }
-  Serial.println("the num of data recieved matches with numberwanted");
-  
-
-  // 这个函数将所有motor调回初始状态
-  //BackToOriginPosition();
-  
-  // MC：实例化的motorcontroler，关于其初始化，参见void setup
-  // 同样地，我不太确定 CJB dl 的接口如何，此处暂时留空。（另外，全局变量不好吗……我觉得弄个structure是画蛇添足）
-  int gimbal_target = MC.gimbal.target;
-  
-  
-  // 为了方便debug我们每次只动一个motor
-  // FUTURE TO DO: 让他们一块转！（虽然效率没有改变但显得更一体化了）
-  
-  // 注：具体输入视receiver的数据而定……我不清楚蓝牙会传回什么样的数据，因此本句无法具体决定
-  StepperMotor(PORT_STEPPER_GIMBAL, res[1], 100, false);
-  // TODO
-  StepperMotor(PORT_STEPPER_BIG_ARM, res[2], 100, false);
-  StepperMotor(PORT_STEPPER_SMALL_ARM, res[3], 100, false);
-  StepperMotor(PORT_STEPPER_HORIZONTAL, res[4], 100, false);
-  
-  // pickup()：指挥夹子将物体夹起来。参数为1或0，具体说明请参见下方函数定义。
-  ServoPickup(0);
-  delay(1000);
-  ServoPickup(1);
-  delay(1000);
-  StepperMotor(PORT_STEPPER_GIMBAL, -res[1], 100, false);
-  // TODO
-  StepperMotor(PORT_STEPPER_BIG_ARM, -res[2], 100, false);
-  StepperMotor(PORT_STEPPER_SMALL_ARM, -res[3], 100, false);
-  StepperMotor(PORT_STEPPER_HORIZONTAL, -res[4], 100, false);
-  delay(1000);
-
-  
-  // 
-  
 }
  
-// 步进电机子函数
-// 函数：StepperMotor    功能：控制步进电机是否脱机、方向、步数
-// 参数：DIRPin---方向控制的pin
-//      ENA---脱机状态，true为脱机 (updated on 20190430)
-//      DIR---方向控制
-//      steps---步进的步数，若steps为0，则电机上电电磁锁死，不转
+// ²½½øµç»ú×Óº¯Êý
+// º¯Êý£ºStepperMotor    ¹¦ÄÜ£º¿ØÖÆ²½½øµç»úÊÇ·ñÍÑ»ú¡¢·½Ïò¡¢²½Êý
+// ²ÎÊý£ºDIRPin---·½Ïò¿ØÖÆµÄpin
+//      ENA---ÍÑ»ú×´Ì¬£¬trueÎªÍÑ»ú (updated on 20190430)
+//      DIR---·½Ïò¿ØÖÆ
+//      steps---²½½øµÄ²½Êý£¬ÈôstepsÎª0£¬Ôòµç»úÉÏµçµç´ÅËøËÀ£¬²»×ª
 //      velocity----TBD
-// 无返回值
+// ÎÞ·µ»ØÖµ
 
 void StepperMotor(int PULPin, boolean DIR, int steps, bool ENA, int velocity=10)
 {
@@ -312,80 +139,223 @@ void StepperMotor(int PULPin, boolean DIR, int steps, bool ENA, int velocity=10)
   }
 }
 
-// 舵机设定角度子函数
-// 函数：ServoMotor     功能：设置舵机的角度
-// 参数：target-----欲要让舵机转到的角度
-//       current-----当前舵机的位置
-// 无返回值
-// WARNING!!!!　
-// current变量一定不能随便乱来，否则可能舵机转的太猛造成机械结构的损坏
+// ¶æ»úÉè¶¨½Ç¶È×Óº¯Êý
+// º¯Êý£ºServoMotor     ¹¦ÄÜ£ºÉèÖÃ¶æ»úµÄ½Ç¶È
+// ²ÎÊý£ºtarget-----ÓûÒªÈÃ¶æ»ú×ªµ½µÄ½Ç¶È
+//       current-----µ±Ç°¶æ»úµÄÎ»ÖÃ
+// ÎÞ·µ»ØÖµ
+// WARNING!!!!¡¡
+// current±äÁ¿Ò»¶¨²»ÄÜËæ±ãÂÒÀ´£¬·ñÔò¿ÉÄÜ¶æ»ú×ªµÄÌ«ÃÍÔì³É»úÐµ½á¹¹µÄËð»µ
 
 void ServoMotor(int target, int current, int velocity=10)
 {
   if (target >= current){
-    for(pos = current; pos<=target; pos++) {    // 每次1°                   
-       servo.write(pos);        // 写角度到舵机     
-       delay(15);                // 延时15ms让舵机转到指定位置
+    for(int pos = current; pos<=target; pos++) {    // Ã¿´Î1¡ã                   
+       myServo.write(pos);        // Ð´½Ç¶Èµ½¶æ»ú     
+       delay(15);                // ÑÓÊ±15msÈÃ¶æ»ú×ªµ½Ö¸¶¨Î»ÖÃ
     } 
   } else {
-    for(pos = current; pos>=target; pos--) {    // 每次1°                   
-       servo.write(pos);        // 写角度到舵机     
-       delay(15);                // 延时15ms让舵机转到指定位置
+    for(int pos = current; pos>=target; pos--) {    // Ã¿´Î1¡ã                   
+       myServo.write(pos);        // Ð´½Ç¶Èµ½¶æ»ú     
+       delay(15);                // ÑÓÊ±15msÈÃ¶æ»ú×ªµ½Ö¸¶¨Î»ÖÃ
     }
   }
 }
 
 
-// 金属夹子函数
-// 函数：PickUp    功能：抓起物品
-// parameter: pick --- 为0时抓起，不为0则放下
+// ½ðÊô¼Ð×Óº¯Êý
+// º¯Êý£ºPickUp    ¹¦ÄÜ£º×¥ÆðÎïÆ·
+// parameter: pick --- Îª0Ê±×¥Æð£¬²»Îª0Ôò·ÅÏÂ
 void ServoPickup(int pick){
   if (pick == 0){
     for (int i = 180; i > 0; i--){
-      myservo.write(i);
+      myServo.write(i);
       delay(15);
     }
   } else {
     for (int i = 0; i<180; i++){
-      myservo.write(i);
+      myServo.write(i);
       delay(15);
     }
   }
 }
 
 
-// 蓝牙模块接收函数
-// 函数：BlueToothReceiver
-// 参数：TBD
-// 返回值：TBD
-void BlueToothReceiver(int numberwanted)
+// À¶ÑÀÄ£¿é½ÓÊÕº¯Êý
+// º¯Êý£ºBlueToothReceiver
+// ²ÎÊý£ºTBD
+// ·µ»ØÖµ£ºTBD
+void BlueToothReceiver(motor_controler mc)
 {
-    // TODO by CJB
-    // an array of data received
-    /*
-    int * res = receiveData(SoftwareSerial bluetooth, byte StartByte, byte InterStart, byte InterEnd, byte EndByte){
-    if (res[0] != numberwanted)
-    {
-      Serial.println("the num of data recieved does not match with numberwanted");
-      return;
-    }
-    Serial.println("the num of data recieved matches with numberwanted");
+    // put your main code here, to run repeatedly:
+
+  // start receiving data
+  
+  byte buff = Serial.read();
+  int num = 0;
+  int sign = 1;
+  ////Serial.println(bluetooth.available());
+  if (buff == StartByte) {
+    ////Serial.println((byte)buff); 
+    //Serial.println(buff);
+    Serial.write(buff);
+    buff = readoff(StartByte, time_de);
+    //buff = Serial.read();
+    num = buff;
+    //Serial.println(num);
+    Serial.write(buff);
+    buff = readoff(num, time_de);
     
-    for (int i = 1; i < res[0]; i++)
-    {
-      
+    int data[num];
+    for(int i = 0; i < num; i++) {
+        buff = readoff(InterStart, time_de);
+        //Serial.print("Sign: ");
+        //Serial.println(buff);
+        sign = buff;
+        Serial.write(sign);
+        
+        buff = readoff(sign, time_de);
+        if (sign == negative) {
+          sign = -1;
+        } else {
+          sign = 1;
+        }
+        int dataabs = buff;
+        data[i] = sign*dataabs;
+        //Serial.println(data[i]); 
+        Serial.write(data[i]);  
+        buff = readoff(dataabs, time_de);
+        //Serial.println("InterEnd");
+        buff = readoff(InterEnd, time_de);
+        //Serial.println("InterEnddone");
+ 
+    }
+    readtill(EndByte, time_de);
+    // print out what we have received to Serial
+    /*
+    //Serial.println("data:");
+    for(int i = 0; i < num; i++) {
+      //Serial.print(data[i]);
+      //Serial.print(" ");
     }
     */
-    
+    //Serial.println();
+    int data_final[num/expansion];
+    for (int i = 0; i < num; i++){
+      int sign = 1;
+      int current_res = 0;
+      for (int j = 0; j < expansion; j++){
+        int current_val = data[i*expansion+j];
+        if (current_val < 0)
+        {
+            sign = -1;
+            current_val = -current_val;
+        }
+        current_res = current_res*100;
+        current_res += current_val;
+      }
+      data_final[i] = sign*current_res;
+    }
+    mc.stepper_big_arm.angle = data_final[0];
+    mc.stepper_small_arm.angle = data_final[1];
+    mc.stepper_gimbal.angle = data_final[2];
+    mc.stepper_horizontal.angle = data_final[3];
+    mc.servo.angle = data_final[4];
+    mc.stepper_big_arm.state = 0;
+    mc.stepper_small_arm.state = 0;
+    mc.stepper_gimbal.state = 0;
+    mc.stepper_horizontal.state = 0;
+    mc.servo.state = 0;
+    Serial.print(mc.stepper_big_arm.angle);
+    Serial.print(" ");
+    Serial.print(mc.stepper_small_arm.angle);
+    Serial.print(" ");
+    Serial.print(mc.stepper_gimbal.angle);
+    Serial.print(" ");
+    Serial.print(mc.stepper_horizontal.angle);
+    Serial.print(" ");
+    Serial.println(mc.servo.angle);
+  }
 }
 
 
-// 返回所有电机的初始位置
-// 函数：BackToOriginPosition()
-// 参数：TBD
-// 返回值：TBD
-// 这个函数将所有电机调回他们的初始位置
+// ·µ»ØËùÓÐµç»úµÄ³õÊ¼Î»ÖÃ
+// º¯Êý£ºBackToOriginPosition()
+// ²ÎÊý£ºTBD
+// ·µ»ØÖµ£ºTBD
+// Õâ¸öº¯Êý½«ËùÓÐµç»úµ÷»ØËûÃÇµÄ³õÊ¼Î»ÖÃ
 void BackToOriginPosition()
 {
     // TODO by CJB
+}
+
+
+void setup()
+{
+  // ²½½øµç»ú
+  // 1ÊÇdir 2ÊÇpul 3ÊÇena
+  // big_arm
+  pinMode (PORT_STEPPER_BIG_ARM, OUTPUT);
+  pinMode (PORT_STEPPER_BIG_ARM+1, OUTPUT);
+  pinMode (PORT_STEPPER_BIG_ARM+2, OUTPUT);
+  pinMode (PORT_STEPPER_SMALL_ARM, OUTPUT);
+  pinMode (PORT_STEPPER_SMALL_ARM+1, OUTPUT);
+  pinMode (PORT_STEPPER_SMALL_ARM+2, OUTPUT);
+  pinMode (PORT_STEPPER_GIMBAL, OUTPUT);
+  pinMode (PORT_STEPPER_GIMBAL+1, OUTPUT);
+  pinMode (PORT_STEPPER_GIMBAL+2, OUTPUT);
+  pinMode (PORT_STEPPER_HORIZONTAL, OUTPUT);
+  pinMode (PORT_STEPPER_HORIZONTAL+1, OUTPUT);
+  pinMode (PORT_STEPPER_HORIZONTAL+2, OUTPUT);
+  pinMode (PORT_SERVO, OUTPUT);
+  pinMode (PORT_SERVO+1, OUTPUT);
+  pinMode (PORT_SERVO+2, OUTPUT);
+
+
+  pinMode(SWITCH_STEPPER_BIG_ARM, INPUT_PULLUP);
+  pinMode(SWITCH_STEPPER_GIMBAL, INPUT_PULLUP);
+  pinMode(SWITCH_STEPPER_HORIZONTAL, INPUT_PULLUP);
+  pinMode(SWITCH_SERVO, INPUT_PULLUP);
+
+  // ¶æ»ú
+  myServo.attach(53);  // ½«Òý½Å9ÉÏµÄ¶æ»úÓëÉùÃ÷µÄ¶æ»ú¶ÔÏóÁ¬½ÓÆðÀ´
+ 
+  //check_zero_state(MC);
+  Serial.begin(9600);
+};
+
+
+
+void loop()
+{
+  // ÎÒÃÇ¼Ù¶¨Õâ¸öº¯Êý¿ÉÒÔ½«À¶ÑÀÄ£¿é´«À´µÄÐÅÏ¢Åª½øÎÒÃÇÔ¤ÏÈÉè¶¨µÄstructÀïÃæ
+  BlueToothReceiver(MC);
+
+  // Õâ¸öº¯Êý½«ËùÓÐmotorµ÷»Ø³õÊ¼×´Ì¬
+  //BackToOriginPosition();
+  
+  // MC£ºÊµÀý»¯µÄmotorcontroler£¬¹ØÓÚÆä³õÊ¼»¯£¬²Î¼ûvoid setup
+  // Í¬ÑùµØ£¬ÎÒ²»Ì«È·¶¨ CJB dl µÄ½Ó¿ÚÈçºÎ£¬´Ë´¦ÔÝÊ±Áô¿Õ¡££¨ÁíÍâ£¬È«¾Ö±äÁ¿²»ºÃÂð¡­¡­ÎÒ¾õµÃÅª¸östructureÊÇ»­ÉßÌí×ã£©
+  int gimbal_target = 60;//MC.gimbal.target;
+  
+  // ÎªÁË·½±ãdebugÎÒÃÇÃ¿´ÎÖ»¶¯Ò»¸ömotor
+  // FUTURE TO DO: ÈÃËûÃÇÒ»¿é×ª£¡£¨ËäÈ»Ð§ÂÊÃ»ÓÐ¸Ä±äµ«ÏÔµÃ¸üÒ»Ìå»¯ÁË£©
+  
+  // ×¢£º¾ßÌåÊäÈëÊÓreceiverµÄÊý¾Ý¶ø¶¨¡­¡­ÎÒ²»Çå³þÀ¶ÑÀ»á´«»ØÊ²Ã´ÑùµÄÊý¾Ý£¬Òò´Ë±¾¾äÎÞ·¨¾ßÌå¾ö¶¨
+  /*
+  StepperMotor(PORT_STEPPER_GIMBAL, 0, 100, false);
+  // TODO
+  StepperMotor(PORT_STEPPER_BIG_ARM, 0, 100, false);
+  StepperMotor(PORT_STEPPER_SMALL_ARM, 0, 100, false);
+  StepperMotor(PORT_STEPPER_HORIZONTAL, 0, 100, false);
+  
+  // pickup()£ºÖ¸»Ó¼Ð×Ó½«ÎïÌå¼ÐÆðÀ´¡£²ÎÊýÎª1»ò0£¬¾ßÌåËµÃ÷Çë²Î¼ûÏÂ·½º¯Êý¶¨Òå¡£
+  ServoPickup(0);
+  delay(1000);
+  ServoPickup(1);
+  delay(1000);
+  
+  // 
+  */
+  delay(500);
 }
